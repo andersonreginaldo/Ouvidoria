29ecd615798e0d1bd1abad3b5fe364740187d0fe
Anderson Reginaldo <anderson.reginaldo@gmail.com>
2022-04-19 14:06:31 -0300

    start backend project

---
+++ backend/ouvidoria/src/main/java/br/senac/ma/OuvidoriaApplication.java
@@ -0,0 +1,13 @@
+package br.senac.ma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuvidoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuvidoriaApplication.class, args);
+	}
+
+}
---
+++ backend/ouvidoria/src/test/java/br/senac/ma/OuvidoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.ma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuvidoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
